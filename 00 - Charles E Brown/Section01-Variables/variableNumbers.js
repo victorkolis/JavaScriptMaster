44ea3317952e7dd7ff8dcb1a35683ad30ab0f949
Victor Kolis <>
2020-11-06 23:40:55 -0300

    20201106

---
+++ 00 - Charles E Brown/Section01-Variables/variableNumbers.js
@@ -0,0 +1,23 @@
+const name = "Victor Kolis"
+let age = 26;
+let a = 3;
+let b = 5;
+
+
+// Addition
+console.log(a + b);
+
+// Subtraction
+console.log(a - b);
+
+// Multiplication
+console.log(a * b);
+
+// Division
+console.log(a / b);
+
+// Modulo
+console.log(a % b);
+
+// Concatenation
+console.log(a + ' ' + name);
---
+++ 00 - Charles E Brown/Section01-Variables/variablesAndStrings.js
@@ -0,0 +1,15 @@
+// Single line comment
+/**
+ * Multile comment
+ */
+
+// First greeting variable
+
+// let is variable
+let greeting = "Hello world";
+let firstName = "Victor";
+let lastName = "Kolis";
+
+
+console.log(greeting);
+console.log(firstName + " " + lastName);
